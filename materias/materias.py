4a3b4e02b692edb92530cff0c990d69fe82fd504
ValCordero8 <l21121188@morelia.tecnm.mx>
2024-09-25 00:06:36 -0600

    T100

---
+++ materias/materias.py
@@ -0,0 +1,10 @@
+class Materia: 
+
+    def __init__(self,nombre: str, maestro: str, descripcion: str, semestre: int, creditos: int):
+        self.nombre = nombre
+        self.maestro = maestro
+        self.descripcion = descripcion
+        self.semestre = semestre
+        self.creditos = creditos
+
+    
